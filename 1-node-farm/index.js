66cd0346e9c59a79f15b586baef1dd2aa9c0b72b
infinity <1185561323@qq.com>
2024-04-27 10:14:27 +0800

    update

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -11,3 +11,32 @@ console.log(textIn);
  */
 const textOut = `This is what we know about the avocado: ${textIn}.\nCteated on ${Date.now}`;
 fs.writeFileSync("./txt/output.txt", textOut);
+
+/**
+ * 读取 / 写入文件（异步）
+ */
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+    if (err) {
+        console.log("ERROR !!!");
+        return;
+    }
+
+    console.log(data1);
+
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+        console.log(data2);
+
+        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile(
+                "./txt/final.txt",
+                `${data2}\n${data3}`,
+                "utf-8",
+                (err) => {
+                    console.log("Your file has been written !");
+                }
+            );
+        });
+    });
+});
